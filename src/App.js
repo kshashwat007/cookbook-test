539185b2abb46c1c73daef463fba0a75af1558ed
kshashwat007 <kshashwat007@gmail.com>
2020-05-29 22:46:06 +0400

    cookbook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const recipes = [
-  {
-    author: "Jim",
-    name: "Chicken Curry",
-    description: "Delicious spicy chicken curry"
-  },
-  {
-    author: "Aravind",
-    name: "Hamburger",
-    description: "Juicy burger with toppings and a soft bun",
-  }
-]
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import Recipe from "./components/Recipe";
+import Alert from "./components/Alert";
+import RecipeFilter from "./components/search-filter";
+import SearchBox from "./components/searchBox";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "c7dad31d";
+  const APP_KEY = "e8a676191da6bfe0d774109b5d4e509f";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+  };
+
+  const filterLogic = e => {
+    e.target.classList.toggle('active-filter');
+    const li = e.target;
+    if (li.classList.contains('active-filter')) {
+      setQuery(li.innerText);
+    } else {
+      setQuery('');
+    }
+  };
+
   return (
     <div className="App">
+      <h1>CookBook</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="search-filter">
+        <RecipeFilter filterLogic={filterLogic}/>
+      </div>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
     </div>
   );
 }
---
+++ src/components/searchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBox = ({ search, updateSearch, placeholder }) => (
+  <div>
+    <input
+      type="text"
+      className="search-bar"
+      value={search}
+      onChange={updateSearch}
+      placeholder={placeholder}
+    />
+    <button type="submit" className="search-button">
+      <i className="fas fa-search" />
+    </button>
+  </div>
+);
+
+export default SearchBox;
